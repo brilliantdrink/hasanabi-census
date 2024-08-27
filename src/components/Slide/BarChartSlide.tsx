4eec88c93f69990d3c689fe6408030b1a75bc949
brilliantdrink <166573010+brilliantdrink@users.noreply.github.com>
2024-08-27 15:12:45 -0700

    fix chart

--- src/components/Slide/BarChartSlide.tsx
+++ src/components/Slide/BarChartSlide.tsx
@@ -1,6 +1,6 @@
 import {createSignal, onMount} from 'solid-js'
 import {Bar} from 'solid-chartjs'
-import {Chart, ChartData, Colors, Legend, plugins, Title, Tooltip} from 'chart.js'
+import {Chart, ChartData, Colors, Legend, LinearScale, plugins, Title, Tooltip} from 'chart.js'
 import DataLabels from 'chartjs-plugin-datalabels'
 
 import styles from './slide.module.scss'
@@ -11,15 +11,15 @@ export default function BarChartSlide({dataFile, title}: { dataFile: string, tit
   const [data, setData] = createSignal<ChartData>(null!)
 
   onMount(() => {
-    Chart.register(Title, Tooltip, Legend, Colors, DataLabels)
+    Chart.register(Title, Tooltip, Legend, Colors, DataLabels, LinearScale)
     getStackedBarData(dataFile).then(setData)
   })
 
   return <>
     <div class={styles.slide}>
       <h2>{title}</h2>
-      <Bar data={data()} options={stackedBarChart} plugins={plugins} height={window.innerHeight * .8}
-           width={window.innerWidth * .6} />
+      <Bar data={data()} options={stackedBarChart} plugins={plugins}
+           height={window.innerHeight * .8} width={window.innerWidth * .6} />
     </div>
   </>
 }
